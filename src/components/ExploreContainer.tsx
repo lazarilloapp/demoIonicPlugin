edc2deecad700587e0667b8643fdd544874b038f
renatojobal <renatojobal@gmail.com>
2022-09-15 10:31:22 -0500

    :boom: update to new Lazarillo Version

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,56 +1,86 @@
 import './ExploreContainer.css';
-import { LazarilloSDK, OpenMapOptions } from "@lzdevelopers/lz-ionic-plugin";
-import { IonButton, IonToast } from '@ionic/react';
-import { useState } from 'react';
+import { LazarilloMap } from '@lzdevelopers/lazarillo-maps';
+import { useRef, useState } from 'react';
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToast,
+} from '@ionic/react';
 
-interface ContainerProps { }
-
-
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const mapRef = useRef<HTMLElement>();
+  let newMap: LazarilloMap;
 
   const [showToast1, setShowToast1] = useState(false);
 
+  async function createMap() {
+    if (!mapRef.current) return;
 
-
-        // Add listener for the map when it is loaded
-    LazarilloSDK.addListener("mapLoaded", () => { setShowToast1(true) })
-
-
-  const apiKey: string = "";
-  const mapOptions : OpenMapOptions = {
-    latitude: 0,
-    longitude: 0,
-    id: "my-map",
-    element: null,
-    parentPlaceId: "-N19VjzEVIj2RDKu7i4r",
-    routingOptions: null
+    newMap = await LazarilloMap.create({
+      id: 'my-cool-map',
+      element: mapRef.current,
+      apiKey: 'AiNFZyJdbr5qa2KHmj7e',
+      config: {
+        center: {
+          lat: 33.6,
+          lng: -117.9,
+        },
+        zoom: 8,
+      },
+    });
   }
-  
-  return (
-    <div className="container">
-      <IonButton expand="full" onClick={() => {      
-          LazarilloSDK.initialize(apiKey);
-          LazarilloSDK.mapSupport(true);
-          LazarilloSDK.openMap(mapOptions);
-       }}>
-          Open the map
-        </IonButton>
-        <IonButton expand="full" onClick={() => {      
-          LazarilloSDK.initialize(apiKey);
-          LazarilloSDK.mapSupport(true);
-          LazarilloSDK.openCustomMap(mapOptions);
-       }}>
-          Open custom map
-        </IonButton>
 
-        <IonToast
-        isOpen={showToast1}
-        onDidDismiss={() => setShowToast1(false)}
-        message="Your settings have been saved."
-        duration={200}
-      />
-    </div>
+  async function addRoute() {
+    if (!newMap) return;
+
+    let initialPos = {
+      building: '-N19VjzEVIj2RDKu7i4r',
+      floor: '-N1OJ6FIVBV6dpjCXEFM',
+      polygons: undefined,
+      latitude: -33.41758007741259,
+      longitude: -70.60615300514021,
+    };
+    let finalPos = {
+      building: '-N19VjzEVIj2RDKu7i4r',
+      floor: '-N1OJ6FIVBV6dpjCXEFM',
+      polygons: undefined,
+      latitude: -33.417596318453455,
+      longitude: -70.60672561495679,
+    };
+
+    newMap.addRoute({
+      mapId: 'my-cool-map',
+      initialPos: initialPos,
+      finalPos: finalPos,
+      initialFloor: '-N1OJ6FIVBV6dpjCXEFM',
+      finalFloor: '-N1OJ6FIVBV6dpjCXEFM',
+      place: '-N19VjzEVIj2RDKu7i4r',
+      preferAccessibleRoute: true,
+    });
+  }
+
+  return (
+    <IonContent>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonRow>
+              <IonButton onClick={createMap}>Open map</IonButton>
+              <IonButton onClick={addRoute}>Add route</IonButton>
+            </IonRow>
+            <capacitor-lazarillo-map ref={mapRef}></capacitor-lazarillo-map>
+            <IonTitle>See places of interest:</IonTitle>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonContent>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { bagOutline } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Testing Lazarillo Plugin</IonTitle>
+          <IonTitle><IonIcon icon={bagOutline}></IonIcon>  Shop</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Testing Lazarillo Plugin</IonTitle>
+            <IonTitle size="large">   Shop</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
