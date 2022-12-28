3c400b1a385d99fdd59d5b2eed52779d1977e0d1
jdariodelgadolasso <111105584+jdariodelgadolasso@users.noreply.github.com>
2022-12-28 15:53:55 -0500

    abc

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -7,6 +7,7 @@ import CoreBluetooth
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    private let locationManager = CLLocationManager()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
